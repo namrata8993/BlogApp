4ddbfa02a769f3e7d49c454d45e6c995a71f715e
unknown <namratamali220@gmail.com>
2024-12-21 13:59:30 +0530

    user api 3 rd commit

--- src/main/java/com/BikkadIt/BlogAppApi/controllers/UserController.java
+++ src/main/java/com/BikkadIt/BlogAppApi/controllers/UserController.java
@@ -1,14 +1,21 @@
 package com.BikkadIt.BlogAppApi.controllers;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.BikkadIt.BlogAppApi.payloads.ApiResponse;
 import com.BikkadIt.BlogAppApi.payloads.UserDto;
 import com.BikkadIt.BlogAppApi.services.UserService;
 
@@ -25,9 +32,38 @@ public class UserController {
 		 UserDto createdUserDto = this.userService.createUser(userDto);
 		 return new ResponseEntity<>(createdUserDto,HttpStatus.CREATED);
 	}
+	 
 	//PUT -update user
+	@PutMapping("/{userId}")
+	 public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto,@PathVariable("userId") Integer uid)
+	 {
+		UserDto updatedUser = this.userService.updateUser(userDto, uid) ;
+		return ResponseEntity.ok(updatedUser);
+	 }
 	
 	//DELETE  - delete user
+	 @DeleteMapping("/{userId}")
+	public ResponseEntity<ApiResponse> deleteUser(@PathVariable("userId")Integer uid)
+	{
+		 this.userService.deleteUser(uid);
+		 return new ResponseEntity(new ApiResponse("User deleted successfully ",true),HttpStatus.OK);
+	}
 	
-	//GET - user get
+	 @GetMapping("/")
+	//GET - all user get
+	 public ResponseEntity<List<UserDto>> getAllUsers()
+	 {
+		 
+		 return ResponseEntity.ok(this.userService.getAllUsers());
+	 }
+	 
+	 @GetMapping("/{userId}")
+		//GET - single user get
+		 public ResponseEntity<UserDto> getSingleUser(@PathVariable Integer userId)
+		 {
+			 
+			 return ResponseEntity.ok(this.userService.getuserById(userId));
+		 }
+		 
+	 
 }
--- src/test/java/com/BikkadIt/BlogAppApi/BlogAppApiApplicationTests.java
+++ src/test/java/com/BikkadIt/BlogAppApi/BlogAppApiApplicationTests.java
@@ -3,11 +3,25 @@ package com.BikkadIt.BlogAppApi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.BikkadIt.BlogAppApi.repositories.UserRepo;
+
 @SpringBootTest
 class BlogAppApiApplicationTests {
 
+	//private UserRepo userRepo;
+	
 	@Test
 	void contextLoads() {
 	}
 
+	//@Test
+	//public void repoTest() {
+		
+		//String classname = this.userRepo.getClass().getName();
+		//String packageName = this.userRepo.getClass().getPackageName();
+		//System.out.println(classname);
+		//System.out.println(packageName);
+		
+	
+//}
 }
